3a7a0d116912a0e74b057175fda13b7690613613
bebossi <bernardobossi77@gmail.com>
2024-02-06 11:16:56 +0000

    lesson 46

---
+++ lessons/46-load-models-with-r3f/src/Experience.jsx
@@ -0,0 +1,34 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+import Model from './Model'
+import { Suspense } from 'react'
+import Placeholder from './Placeholder'
+import Hamburguer from './Hamburguer'
+import Fox from './Fox'
+
+export default function Experience() {
+  //   const model = useLoader(GLTFLoader, './hamburger.glb')
+
+  return (
+    <>
+      <Perf position="top-left" />
+      <OrbitControls makeDefault />
+      <directionalLight
+        castShadow
+        position={[1, 2, 3]}
+        intensity={4.5}
+        shadow-normalBias={0.04}
+      />
+      <ambientLight intensity={1.5} />
+      <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      <Suspense fallback={<Placeholder position-y={0.5} scale={[2, 3, 2]} />}>
+        <Hamburguer scale={0.35} />
+      </Suspense>
+      <Fox />
+    </>
+  )
+}
